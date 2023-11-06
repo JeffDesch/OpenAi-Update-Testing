d898fa3d6105d05a388133f2e47902be909b7168
BrokenShell <webmaster@sharpdesigndigital.com>
2023-11-06 14:24:33 -0800

    JSON mode testing in main

---
+++ list_models.py
@@ -0,0 +1,15 @@
+import os
+
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+    # organization=os.environ.get("OPENAI_ORG_KEY"),
+)
+
+models = client.models.list()
+output = [model for model in models if "gpt" in model.id]
+print(*output, sep="\n")
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
+import json
 import os
 
 from openai import OpenAI
 from dotenv import load_dotenv
-
 
 load_dotenv()
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
+user_prompt = """Create a character for an RPG game, choose a name, class, age, level, and finally write a short visual description.
+Follow this example:
+
+{
+    "name": <name>, 
+    "class": <class>, 
+    "age": <age>, 
+    "level": <level>, 
+    "description": <description>
+}
+"""
 chat_completion, *_ = client.chat.completions.create(
-    messages=[{"role": "user", "content": "Say this is a test"}],
-    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "Please output valid JSON"},
+        {"role": "user", "content": user_prompt}],
+    model="gpt-4-1106-preview",
+    response_format={"type": "json_object"},
 ).choices
-reply = chat_completion.message.content
+content = chat_completion.message.content
+print(content)
+reply = json.loads(content)
 print(reply)
