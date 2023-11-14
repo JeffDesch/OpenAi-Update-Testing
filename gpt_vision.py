027f37136de8a485b91fc3148e5a7c5df533c630
Jeff Desch <jeff.desch@gmail.com>
2023-11-14 17:49:06 -0600

    added multi-image support, logging of results to .txt file

--- gpt_vision.py
+++ gpt_vision.py
@@ -9,32 +9,44 @@ class GPTVision:
     load_dotenv()
     api_key = os.getenv("OPENAI_API_KEY")
 
-    def __call__(self, image_path: str):
-        # Getting the base64 string
-        base64_image = self.encode_image(image_path)
+    def __call__(self, image_paths: list[str]):
 
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.api_key}"
         }
 
+        if len(image_paths) == 1:
+            content = [
+                {
+                    "type": "text",
+                    "text": "OCR this image for me. Do not warn about any difficulties interpreting the image."
+                }
+            ]
+        else:
+            content = [
+                {
+                    "type": "text",
+                    "text": "OCR these images for me and concatenate the results together. Do not warn about any difficulties interpreting the images."
+                }
+            ]
+
+        for image_path in image_paths:
+            base64_image = self.encode_image(image_path)
+            img_dict = {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/jpeg;base64,{base64_image}"
+                }
+            }
+            content.append(img_dict)
+
         payload = {
             "model": "gpt-4-vision-preview",
             "messages": [
                 {
                     "role": "user",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": "OCR this image for me."
-                        },
-                        {
-                            "type": "image_url",
-                            "image_url": {
-                                "url": f"data:image/jpeg;base64,{base64_image}"
-                            }
-                        }
-                    ]
+                    "content": content
                 }
             ],
             "max_tokens": 2048,
@@ -52,4 +64,3 @@ class GPTVision:
     def encode_image(image_path):
         with open(image_path, "rb") as image_file:
             return base64.b64encode(image_file.read()).decode('utf-8')
-
--- vision_test.py
+++ vision_test.py
@@ -1,23 +1,48 @@
 import os
+import json
+from pathlib import Path
 from gpt_vision import GPTVision
 
 
-def get_path(code: str):
-    folder = code[:2] + "--"
+def get_folder_path(root: str, code: str):
+    sub_folder = code[:2] + "--"
+    folder_path = os.path.join(root, sub_folder, code)
 
-    if os.path.exists(f"test_stories/{folder}/{code}"):
-        if os.path.exists(f"test_stories/{folder}/{code}/Photo {code}.jpg"):
-            return f"test_stories/{folder}/{code}/Photo {code}.jpg"
-        elif os.path.exists(f"test_stories/{folder}/{code}/Photo {code} pg1.jpg"):
-            return f"test_stories/{folder}/{code}/Photo {code} pg1.jpg"
+    if os.path.exists(folder_path):
+        return folder_path
     else:
         return ""
 
 
+def get_image_paths(folder_path: str):
+    paths = []
+    if os.path.exists(folder_path):
+        images = Path(folder_path).glob('*.jpg')
+        for image in images:
+            paths.append(os.path.normpath(image))
+    return paths
+
+
+def write_results(ocr_response, folder_path):
+    response_data = ocr_response.json()
+    with open(os.path.join(folder_path, 'ocr_results.txt'), 'at+') as file:
+        file.write(f"'created': {response_data['created']}\n")
+        file.write(f"'model': {response_data['model']}\n")
+        file.write(f"'usage': {str(response_data['usage'])}\n\n")
+        file.write(response_data['choices'][0]['message']['content'])
+        file.write("\n")
+
+
 if __name__ == '__main__':
-    test_no = "3212"
+
     ocr = GPTVision()
-    img_path = get_path(test_no)
-    if img_path:
-        response = ocr(img_path)
+    test_root = "test_stories"
+    test_no = "3212"
+    folder = get_folder_path(test_root, test_no)
+
+    if folder:
+        image_paths = get_image_paths(folder)
+        response = ocr(image_paths)
         print(response.json())
+        write_results(response, folder)
+
